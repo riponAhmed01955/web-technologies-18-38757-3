a91e21aaa871ef120d9e8c1cf4b747fb7d2289bc
riponAhmed01955 <106858022+riponAhmed01955@users.noreply.github.com>
2022-08-23 15:04:20 +0600

    Add files via upload

---
+++ lab task 6/templates/footer.php
@@ -0,0 +1,10 @@
+<?php
+function footer_section()
+{
+?>
+    <footer>
+        <h2> Designed by <a href="https://github.com/Ripon-hossain /ripon-hossain.git">Ripon Ahmed </a></h2>
+    </footer>
+    </body>
+
+    </html><?php } ?>
